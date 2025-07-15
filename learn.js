075d7d3af831450e29ae50c49908497802f052ba
Данила <absatarov7405@mail.ru>
2025-07-15 11:41:57 +0500

    js - функции - 1

--- learn.js
+++ learn.js
@@ -1,1 +1,47 @@
 // Обучение js
+
+// function 
+
+function adminStatus(age,name){
+    alert("Hello!")
+    console.log(`Code is good!${"Возраст: "+age+' '+ "Имя: "+name}`);
+}
+adminStatus(12,"Oleg")
+function letLearn(status){
+    switch(status){
+        case 1:
+            result = "Ok";
+            break;
+        case 0:
+            result = "Error";
+            break;
+        default:
+            result = "Ошибка на сервере! Повторите попытку позже!";
+            break;
+    }
+    alert(result);
+}
+letLearn(1)
+// Внешние переменные
+const userNameAdmin = 'Nikita';
+
+function adminName() {
+    alert(`Здравствуйте!${" "+userNameAdmin}`)
+    
+}
+adminName();
+
+let userName = 'Вася';
+
+function showMessage() {
+  userName = "Петя"; // (1) изменяем значение внешней переменной
+
+  let message = 'Привет, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // Вася перед вызовом функции
+
+showMessage();
+
+alert( userName ); // Петя, значение внешней переменной было изменено функцией
